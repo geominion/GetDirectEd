5e4c714a5c995f3543b95a00af132cd2ecf130ed
Eric Klinger <eric.klinger@gmail.com>
2012-04-09 15:38:52 -0600

    Added the SubActivity model class

---
+++ GD/src/appEd/getDirectEd/model/SubActivity.java
@@ -0,0 +1,84 @@
+package appEd.getDirectEd.model;
+/**
+ * @author Eric Klinger
+ * 
+ 
+ */
+public class SubActivity{
+
+	/**
+	 *   Variables
+	 * id          - 
+	 * name        - 
+	 * superType   -
+	 * description - 
+	 */
+	private long   id;
+	private String name;
+	private String description;
+	private Integer superTypeID;
+
+	
+	/**
+	 * Activity generation requires no specialized inputs
+	 */
+	public SubActivity() {
+		//TODO: any initialization required
+	}
+
+	/**
+	 * @return id
+	 */
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return subType
+	 */
+	public Integer getSuperType() {
+		return superTypeID;
+	}
+
+	/**
+	 * @param subType 
+	 */
+	public void setSuperType(Integer superTypeID) {
+		this.superTypeID = superTypeID;
+	}
+
+	/**
+	 * @return description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}	
+}
