ddbebabe5dd3bbdbc4d29f1231b83a1416c00171
Joshua Dunsmuir <geojbd@gmail.com>
2012-04-10 16:13:10 -0600

    couple views were bad.

---
+++ GD/src/appEd/getDirectEd/main/FacilityDetailsActivity.java
@@ -0,0 +1,16 @@
+package appEd.getDirectEd.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FacilityDetailsActivity extends Activity {
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.facility_results);
+        
+        
+	}
+}
