9f1b4c2c03b91c8996d652b4175920b84e60d74c
Joshua Dunsmuir <geojbd@gmail.com>
2012-04-21 17:07:41 -0600

    button button

--- GD/src/appEd/getDirectEd/main/FacilitiesActivity.java
+++ GD/src/appEd/getDirectEd/main/FacilitiesActivity.java
@@ -4,6 +4,9 @@ import android.app.TabActivity;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TabHost;
 
 public class FacilitiesActivity extends TabActivity {
@@ -32,6 +35,14 @@ public class FacilitiesActivity extends TabActivity {
         tabHost.addTab(spec);
 
         tabHost.setCurrentTab(2);
+        
+        Button button1 = (Button)findViewById(R.id.button1);
+		button1.setOnClickListener(new OnClickListener() {
+			public void onClick(View arg0) {
+				Intent intent = new Intent(FacilitiesActivity.this, GetDirectEdActivity.class);
+            	startActivity(intent);       
+			}
+		});
 	}
 
 }
